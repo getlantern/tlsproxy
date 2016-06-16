fbbf2acde5b063a9834491db5a2bf688bb526401
Ox Cart <ox.to.a.cart@gmail.com>
2016-06-15 21:54:31 -0500

    Initial checkin

---
+++ tlsproxy.go
@@ -0,0 +1,132 @@
+// tlsproxy provides a TLS proxy kind of like stunnel
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"net"
+	"os"
+	"time"
+
+	"github.com/getlantern/golog"
+	"github.com/getlantern/keyman"
+	"github.com/getlantern/netx"
+	"github.com/oxtoacart/bpool"
+)
+
+var (
+	log = golog.LoggerFor("tlsproxy")
+
+	mode        = flag.String("mode", "server", "Mode.  server = listen for TLS connections, client = listen for plain text connections")
+	hostname    = flag.String("hostname", "", "Hostname to use for TLS. If not supplied, will auto-detect hostname")
+	listenAddr  = flag.String("listen-addr", ":6380", "Address at which to listen for incoming connections")
+	forwardAddr = flag.String("forward-addr", "localhost:6379", "Address to which to forward connections")
+	pkfile      = flag.String("pkfile", "pk.pem", "File containing private key for this proxy")
+	certfile    = flag.String("certfile", "cert.pem", "File containing the certificate for this proxy")
+	cafile      = flag.String("cafile", "cert.pem", "File containing the certificate authority (or just certificate) with which to verify the remote end's identity")
+	help        = flag.Bool("help", false, "Get usage help")
+
+	buffers = bpool.NewBytePool(1000, 32768)
+)
+
+func main() {
+	flag.Parse()
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	hostname := *hostname
+	if hostname == "" {
+		_hostname, err := os.Hostname()
+		if err == nil {
+			hostname = _hostname
+		}
+	}
+	if hostname == "" {
+		hostname = "localhost"
+	}
+
+	log.Debugf("Mode: %v", *mode)
+	log.Debugf("Hostname: %v", hostname)
+	log.Debugf("Forwarding to: %v", *forwardAddr)
+
+	cert, err := keyman.KeyPairFor(hostname, *pkfile, *certfile)
+	if err != nil {
+		log.Fatalf("Unable to load keypair: %v", err)
+	}
+	ca, err := keyman.LoadCertificateFromFile(*cafile)
+	if err != nil {
+		log.Fatalf("Unable to load ca certificate: %v", err)
+	}
+	pool := ca.PoolContainingCert()
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		RootCAs:      pool,
+		ClientCAs:    pool,
+	}
+
+	switch *mode {
+	case "server":
+		runServer(tlsConfig)
+	case "client":
+		runClient(tlsConfig)
+	default:
+		log.Fatalf("Unknown mode: %v", *mode)
+	}
+}
+
+func runServer(tlsConfig *tls.Config) {
+	doRun(func() (net.Listener, error) {
+		return tls.Listen("tcp", *listenAddr, tlsConfig)
+	}, func() (net.Conn, error) {
+		return net.DialTimeout("tcp", *forwardAddr, 30*time.Second)
+	})
+}
+
+func runClient(tlsConfig *tls.Config) {
+	host, _, err := net.SplitHostPort(*forwardAddr)
+	if err != nil {
+		log.Fatalf("Unable to determine hostname for server: %v", err)
+	}
+	tlsConfig.ServerName = host
+
+	doRun(func() (net.Listener, error) {
+		return net.Listen("tcp", *listenAddr)
+	}, func() (net.Conn, error) {
+		return tls.Dial("tcp", *forwardAddr, tlsConfig)
+	})
+}
+
+func doRun(listen func() (net.Listener, error), dial func() (net.Conn, error)) {
+	l, err := listen()
+	if err != nil {
+		log.Fatalf("Unable to listen: %v", err)
+	}
+	defer l.Close()
+	log.Debugf("Listening for incoming connections at: %v", l.Addr())
+
+	for {
+		in, err := l.Accept()
+		if err != nil {
+			log.Fatalf("Unable to accept: %v", err)
+		}
+		go func() {
+			defer in.Close()
+			out, err := dial()
+			if err != nil {
+				log.Debugf("Unable to dial forwarding address: %v", err)
+				return
+			}
+			defer out.Close()
+
+			bufOut := buffers.Get()
+			bufIn := buffers.Get()
+			defer buffers.Put(bufOut)
+			defer buffers.Put(bufIn)
+			netx.BidiCopy(out, in, bufOut, bufIn)
+		}()
+	}
+}
